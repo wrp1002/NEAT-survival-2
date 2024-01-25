dd6363d81518f970c945dbf5c9779448aff15426
wrp1002 <wes.hamster@gmail.com>
2024-01-24 20:08:15 -0500

    added LiveObject

---
+++ src/Objects/LiveObject.cpp
@@ -0,0 +1,23 @@
+#include "LiveObject.h"
+
+LiveObject::LiveObject() : Object() {
+	polymorphic_id = "LiveObject";
+	health = 0;
+	maxHealth = 0;
+}
+
+LiveObject::LiveObject(b2Vec2 pos, b2Vec2 pixelSize, float angle, ALLEGRO_COLOR color, int shapeType, double health) : Object(pos, pixelSize, angle,color, shapeType) {
+	polymorphic_id = "LiveObject";
+	this->health = health;
+}
+
+double LiveObject::TakeDamage(double amount) {
+	amount = min(amount, health);
+	this->health -= amount;
+
+	return amount;
+}
+
+double LiveObject::GetHealth() {
+	return health;
+}
---
+++ src/Objects/LiveObject.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <allegro5/color.h>
+
+#include <Box2D/Box2D.h>
+
+#include <memory>
+#include <string>
+
+#include "Object.h"
+
+using namespace std;
+
+class LiveObject : public Object {
+	protected:
+		double health, maxHealth;
+
+	public:
+		enum SHAPE_TYPES {RECT, CIRCLE};
+
+		LiveObject();
+		LiveObject(b2Vec2 pos, b2Vec2 pixelSize, float angle, ALLEGRO_COLOR color, int shapeType, double health);
+
+		virtual void Update() {
+			if (health <= 0)
+				alive = false;
+		}
+
+		double TakeDamage(double amount);
+
+		double GetHealth();
+};
+
