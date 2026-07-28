649581079353d7978650389880c2a9cbce1b7a6a
wrp1002 <wes.hamster@gmail.com>
2026-07-28 18:51:38 -0400

    globals: added mutation params, collected other hard coded values to here

--- src/Globals.h
+++ src/Globals.h
@@ -16,9 +16,28 @@ namespace Globals {
 
 	const string resourcesDir = "Resources/";
 
+	const int MAX_OBJECT_SIZE = 50;
+
 	const int GENE_LENGTH = 16;
+	const int GENE_INSTRUCTION_TYPES = 5;
 	const float GENE_MUTATE_CHANCE = 0.01;
 	const float GENE_CREATE_CHANCE = 0.001;
 	const float GENE_DELETE_CHANCE = 0.001;
+
+	const struct MutationProfile {
+		static constexpr float MAX_HEADER_MUTATION_RATE = 0.001f;
+		static constexpr float MAX_PARAMETER_MUTATION = 0.02f;      // 1% per parameter
+		static constexpr float MAX_PARAMETER_REWRITE = 0.01f;      // 1% per parameter
+		static constexpr float MAX_DUPLICATION = 0.002f;            // 0.2% per instruction
+		static constexpr float MAX_DELETION = 0.001f;
+		static constexpr float MAX_MOVE = 0.001f;
+		static constexpr float MAX_SWAP = 0.001f;
+		static constexpr float MAX_RANDOM_INSERT = 0.0002f;
+	} mutationProfile;
+
+	const unsigned int STARTING_POPULATION = 400;
+	const unsigned int POPULATION_MIN_SAFETY_NET = 50;
+	const unsigned int STARTING_DNA_GENE_LENGTH = 50;
+	const unsigned int HEADER_GENES_COUNT = 2;
 
 }
